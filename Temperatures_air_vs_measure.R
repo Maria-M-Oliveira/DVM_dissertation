e9f295e7d4fdb67805882cc424683b5f98831e28
Maria <oliveira.maria.miguel@gmail.com>
2022-12-22 20:18:19 +0000

    Update Temperatures_air_vs_measure.R

--- Temperatures_air_vs_measure.R
+++ Temperatures_air_vs_measure.R
@@ -1,4 +1,6 @@
-library(weatherData)
+# library(weatherData)
 # weatherdata package is currently not working (: need to try another
 
-getWeatherForDate(station_id = "LPPT" ,"2022-01-02", end_date = "2022-03-01")
+# getWeatherForDate(station_id = "LPPT" ,"2022-01-02", end_date = "2022-03-01")
+library(rgee)
+ee_Initialize()
