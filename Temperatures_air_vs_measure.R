16ab898f3fb7ba1c0cfcc4db7781132d5e0ce23a
Maria Oliveira <oliveira.maria.miguel@gmail.com>
2022-12-25 12:13:55 +0000

    Update google earth engine

--- Temperatures_air_vs_measure.R
+++ Temperatures_air_vs_measure.R
@@ -1,4 +1,9 @@
 library(weatherData)
 # weatherdata package is currently not working (: need to try another
+# getWeatherForDate(station_id = "LPPT" ,"2022-01-02", end_date = "2022-03-01")
 
-getWeatherForDate(station_id = "LPPT" ,"2022-01-02", end_date = "2022-03-01")
+# Usar outra library
+library(rgee)
+
+ee_Initialize()
+ee_install_upgrade()
