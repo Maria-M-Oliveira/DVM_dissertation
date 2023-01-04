2f730ee49bd868ef7085824250b09d914652193a
Maria Oliveira <oliveira.maria.miguel@gmail.com>
2023-01-04 12:05:56 +0000

    Adicionei codigo com extraçao temperaturas
    Ainda esta muito "basico" pq nao é com a regiao que quero, mas sim as regioes do exemplo so para testar, mas we going somewhere

--- Temperatures_air_vs_measure.R
+++ Temperatures_air_vs_measure.R
@@ -1,6 +1,9 @@
 library(rgee)
 library(googledrive)
 library(sf)
+library(tidyverse)
+library(ggplot2)
+
 
 # Library a usar para earth engine= rgee
 # Guia instalacao - https://github.com/r-spatial/rgee
@@ -16,28 +19,40 @@ rgee::ee_Initialize()
 
 #####################
 # codigo de teste do pacote
-srtm <- rgee::ee$Image("USGS/SRTMGL1_003")
 
-viz <- list(
-  max = 4000,
-  min = 0,
-  palette = c("#000000","#5AAD5A","#A9AD84","#FFFFFF")
-)
+nc <- st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE)
 
-rgee::Map$addLayer(
-  eeObject = srtm,
-  visParams =  viz,
-  name = 'SRTM',
-  # legend = TRUE
-)
-#####################
+terraclimate <- ee$ImageCollection("IDAHO_EPSCOR/TERRACLIMATE") %>%
+  ee$ImageCollection$filterDate("2001-01-01", "2002-01-01") %>%
+  ee$ImageCollection$map(function(x) x$select("pr")) %>% # Select only precipitation bands
+  ee$ImageCollection$toBands() %>% # from imagecollection to image
+  ee$Image$rename(sprintf("PP_%02d",1:12)) # rename the bands of an image
+
+ee_nc_rain <- ee_extract(
+  x = terraclimate,
+  y = nc["NAME"],
+  scale = 250,
+  fun = ee$Reducer$mean(),
+  sf = TRUE
+)  %>% 
+  pivot_longer(PP_01:PP_12, names_to = "month", values_to = "pr") %>%
+  mutate(month, month=gsub("PP_", "", month))
+
+ee_nc_rain %>%
+  ggplot(aes(x = month, y = pr, group = NAME, color = pr)) +
+  geom_line(alpha = 0.4) +
+  xlab("Month") +
+  ylab("Precipitation (mm)") +
+  theme_minimal()
 
 ####################
 # Guia a seguir: https://developers.google.com/earth-engine/tutorials/community/ph-ug-temp
 ###
 # 1o definir borders, queria so peniche, mas nao vamos ser muito ambiciosos logo portanto definir PT
 # Image collection a usar para definir borders:
-# https://developers.google.com/earth-engine/datasets/catalog/USDOS_LSIB_SIMPLE_2017
+# https://developers.google.com/earth-engine/datasets/catalog/USDOS_LSIB_SIMPLE_2017 --> este para pais
+# https://developers.google.com/earth-engine/datasets/catalog/FAO_GAUL_2015_level1#description --> 1º nivel
+# https://developers.google.com/earth-engine/datasets/catalog/FAO_GAUL_2015_level2#description --> 2º nivel
 ###
 # Image collection a usar com daily temps:
 # ee.ImageCollection("ECMWF/ERA5/DAILY") 
@@ -56,3 +71,36 @@ rgee::Map$addLayer(
 # Quero graph time series com duas linhas temp
 # Preciso testes estat para comparar os dois - var quantitativas
 
+#############
+# Inicio do codigo a serio
+
+temps_ar <- ee$ImageCollection("ECMWF/ERA5/DAILY") %>%
+  ee$ImageCollection$filterDate("2017-01-02", "2022-02-01") %>%
+  ee$ImageCollection$map(function(x) x$select("mean_2m_air_temperature")) %>% # Select only precipitation bands
+  ee$ImageCollection$toBands() # from imagecollection to image
+
+ee_nc_temp <- ee_extract(
+    x = temps_ar,
+    y = nc["NAME"],
+    scale = 250,
+    fun = ee$Reducer$mean(),
+    sf = TRUE
+  )  
+# Ficamos com df com colunas Xyyyymmdd_mean_2m_air_temperature -> portanto colunas desde dia 2/1/2017 a 1/2/2022
+# Which means, preciso de pegar no pivot_longer e por isto como deve ser, em que as rows sao datas
+
+ee_nc_temp <- ee_nc_temp %>% 
+  pivot_longer(X20170102_mean_2m_air_temperature:X20200709_mean_2m_air_temperature, names_to = "day", values_to = "K") %>%
+  mutate(day, day=gsub("X", "", day)) %>% 
+  mutate(day, day=gsub("_mean_2m_air_temperature", "", day)) 
+# Erro 1: so extraiu ate 09-07-2020 por alguma razao, pensar what do and why
+
+ee_nc_temp %>%
+  ggplot(aes(x = day, y = K, group = NAME, color = K)) +
+  geom_line(alpha = 0.4) +
+  xlab("Day") +
+  ylab("Temperature (K)") +
+  theme_minimal()
+# Funciona, demora bues pq tenho 128500 observacoes pq  nao e so uma localidade
+# Quando substituir por so uma, vai correr mais rapido
+# Point is, funciona
