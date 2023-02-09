a0b8b7e2bbeee36166c73e05eb43cee2549bfdf8
Maria <oliveira.maria.miguel@gmail.com>
2023-02-09 00:06:27 +0000

    mini update de quem está doente

--- Temperatures_air_vs_measure.R
+++ Temperatures_air_vs_measure.R
@@ -17,37 +17,6 @@ library(ggplot2)
 #  3 - conda nao encontra gcloud instalado - https://gis.stackexchange.com/questions/445457/gcloud-command-not-found-when-authenticating-google-earth-engine
 #  4 - R nao encontra credenciais/gcloud - https://github.com/r-spatial/rgee/issues/269#issuecomment-1246880456
 
-rgee::ee_Initialize()
-
-#####################
-# codigo de teste do pacote
-
-nc <- st_read("Cont_AAD_CAOP2020", geometry_column = "geometry") %>% 
-  subset(Concelho %in% c("Peniche"))
-
-terraclimate <- ee$ImageCollection("IDAHO_EPSCOR/TERRACLIMATE") %>%
-  ee$ImageCollection$filterDate("2001-01-01", "2002-01-01") %>%
-  ee$ImageCollection$map(function(x) x$select("pr")) %>% # Select only precipitation bands
-  ee$ImageCollection$toBands() %>% # from imagecollection to image
-  ee$Image$rename(sprintf("PP_%02d",1:12)) # rename the bands of an image
-
-ee_nc_rain <- ee_extract(
-  x = terraclimate,
-  y = nc["NAME"],
-  scale = 250,
-  fun = ee$Reducer$mean(),
-  sf = TRUE
-)  %>% 
-  pivot_longer(PP_01:PP_12, names_to = "month", values_to = "pr") %>%
-  mutate(month, month=gsub("PP_", "", month))
-
-ee_nc_rain %>%
-  ggplot(aes(x = month, y = pr, group = NAME, color = pr)) +
-  geom_line(alpha = 0.4) +
-  xlab("Month") +
-  ylab("Precipitation (mm)") +
-  theme_minimal()
-
 ####################
 # Guia a seguir: https://developers.google.com/earth-engine/tutorials/community/ph-ug-temp
 # https://cran.r-project.org/web/packages/rgee/rgee.pdf
@@ -68,10 +37,7 @@ ee_nc_rain %>%
 ###
 # Converter temp de  para ºC
 ###
-# Exportar ficheiro temps como csv bc why not, pode dar jeito
-###
 # Mapear como uma time series as temperaturas a considerar
-
 
 ############
 ### Analise temp bss vs temp ar diarias
@@ -80,22 +46,15 @@ ee_nc_rain %>%
 
 #############
 # Inicio do codigo a serio
+rgee::ee_Initialize()
 
+#####################
+nc <- st_read("Cont_AAD_CAOP2020", geometry_column = "geometry") %>% 
+  subset(Concelho %in% c("Peniche"))
 temps_ar <- ee$ImageCollection("ECMWF/ERA5/DAILY") %>%
   ee$ImageCollection$filterDate("2017-01-02", "2022-02-01") %>%
-  ee$ImageCollection$map(function(x) x$select("mean_2m_air_temperature")) %>% # Select only precipitation bands
+  ee$ImageCollection$map(function(x) x$select("mean_2m_air_temperature")) %>% # Select only temperature bands
   ee$ImageCollection$toBands() # from imagecollection to image
-
-peniche <- ee$FeatureCollection("FAO/GAUL/2015/level2") %>% 
-  ee$FeatureCollection$select("ADM1_NAME", "ADM1_CODE", "Shape_Area", "Shape_Length")$filter("ADM1_NAME"="Leiria") %>% 
-  ee$FeatureCollection$getInfo()
-# Da erro, nao e bem isto
-# https://csaybar.github.io/rgee-examples/#Geometry%2c%20Feature%2c%20FeatureCollection --> investigar
-# Tenho de transformar isto num objeto sf para conseguir fazer alguma coisa dele ACHO
-# Mas queria so a infor de leiria,o resto pouco me interessa, how to escolher so leiria
-
-# Outra coisa que posso fazer, e ir pelos dados portugeses mesmo, ja tenho shp com esses dados algures
-# Provavelmente ia dar ao mesmo --> tentar
 
 ee_nc_temp <- ee_extract(
     x = temps_ar,
@@ -104,14 +63,16 @@ ee_nc_temp <- ee_extract(
     fun = ee$Reducer$mean(),
     sf = TRUE
   )  
-# Ficamos com df com colunas Xyyyymmdd_mean_2m_air_temperature -> portanto colunas desde dia 2/1/2017 a 1/2/2022
+
+
+# Ficamos com df com colunas Xyyyymmdd_mean_2m_air_temperature -> portanto colunas desde dia 2/1/2017 a 9/7/2020 pq era o que tinhamos no earth engine
 # Which means, preciso de pegar no pivot_longer e por isto como deve ser, em que as rows sao datas
 
 ee_nc_temp <- ee_nc_temp %>% 
   pivot_longer(X20170102_mean_2m_air_temperature:X20200709_mean_2m_air_temperature, names_to = "day", values_to = "K") %>%
   mutate(day, day=gsub("X", "", day)) %>% 
   mutate(day, day=gsub("_mean_2m_air_temperature", "", day)) 
-# Erro 1: so extraiu ate 09-07-2020 por alguma razao, pensar what do and why
+
 
 ee_nc_temp %>%
   ggplot(aes(x = day, y = K, color = K)) +
@@ -120,5 +81,10 @@ ee_nc_temp %>%
   ylab("Temperature (K)") +
   theme_minimal()
 
-# Funciona assim, mas...
-# Nao percebo nada do grafico que ta aqui
+temp_date <- ee_nc_temp %>%  
+  st_drop_geometry()
+# transformar coluna day em datas e converter coluna K em celsius
+# uplaod da DB da tese com temperaturas e datas
+# juntar as duas com cuidado para ter coluna temp ar e coluna temp produto
+# avaliar correlaçao ? como?
+# time series depois
